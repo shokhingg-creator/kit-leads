2ed4106780af5596cee5373d0532a0fc2c60697e
shokhingg-creator <shokhin.g.g@gmail.com>
2026-08-17 10:25:01 +0300

    Load additional KIT tender searches

--- tender-extension/background.js
+++ tender-extension/background.js
@@ -1,8 +1,10 @@
+importScripts("search-config.js");
 const SOURCES=[
 {name:"ЕИС",url:"https://zakupki.gov.ru/epz/order/extendedsearch/results.html"},
 {name:"Росэлторг",url:"https://www.roseltorg.ru/procedures/search"},
 {name:"B2B-Center",url:"https://www.b2b-center.ru/market/?f_keyword=%D0%B4%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D0%B0&searching=1&company_type=2&price_currency=0&date=1&trade=all#search-result"},
 {name:"Фабрикант",url:"https://www.fabrikant.ru/trades/procedure/search/"}];
+if(Array.isArray(globalThis.KIT_EXTRA_SOURCES)) SOURCES.push(...globalThis.KIT_EXTRA_SOURCES);
 chrome.runtime.onInstalled.addListener(()=>chrome.alarms.create("kit-auto-collect",{periodInMinutes:30}));
 chrome.alarms.onAlarm.addListener(a=>{if(a.name==="kit-auto-collect")collectAll()});
 chrome.runtime.onMessage.addListener((m,s,r)=>{
